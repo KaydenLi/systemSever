5d49190a9cb5c7e6ee3795e7cbecf6a27748a3be
KaydenLi <820807451@qq.com>
2020-04-07 18:17:49 +0800

    新增传感器schema

---
+++ models/Sensor.js
@@ -0,0 +1,33 @@
+// import mongoose from 'mongoose'
+const mongoose = require('mongoose')
+
+//定义项目schema，存储所有项目
+const schema = new mongoose.Schema({
+    projectId: { type: String },
+    name: { type: String },
+    img: { type: String },
+    currenttime: { type: Date },
+    timeStamp: { type: String },
+    children: [{
+        name: { type: String },
+        machineinfo: { type: String },
+        value: [
+            {
+                id: { type: String },
+                notes: { type: String },
+                ratio: { type: Number },
+                limit: { type: Number },
+                threshold: { type: Number },
+                position: { type: Array },
+                initialError: { type: Number },
+                name: { type: String },
+                type: { type: String },
+                group: { type: Array },
+                unit: { type: String },
+                value: { type: Array }
+            }
+        ]
+    }]
+})
+
+module.exports = mongoose.model('Sensor', schema)
