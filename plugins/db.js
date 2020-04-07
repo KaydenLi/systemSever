80e92f66ddeaf08dbe6f84f9e0d7ce624b2d8b02
KaydenLi <820807451@qq.com>
2020-04-07 18:15:44 +0800

    做出部分修改

--- plugins/db.js
+++ plugins/db.js
@@ -1,7 +1,9 @@
-module.exports =  app => {
+module.exports = app => {
     const mongoose = require('mongoose')
     mongoose.connect('mongodb://127.0.0.1:27017/monitorSystem', {
         useNewUrlParser: true,
-        useUnifiedTopology: true
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
     })
 }
