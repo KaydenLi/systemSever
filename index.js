5be3c9ded7752153a89d5d16250cf96117010dcd
KaydenLi <820807451@qq.com>
2020-04-16 17:24:39 +0800

    增加路由

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const errorHandler = require('./middlewares/error_middleware')
 app.use(express.json())
 //允许跨域
 app.use(require('cors')())
-
+// 静态文件托管
+app.use('/uploads', express.static(__dirname + '/uploads'))
 //前端请求路由
 require('./plugins/db')(app)
 require('./routes/web/index')(app)
